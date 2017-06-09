7dcd32a05ad3ef5d39cbab70b3855cfa6da56309
r4mstein <r4mstein@ukr.net>
2017-06-09 15:18:11 +0300

    add login activity

---
+++ app/src/main/java/ua/r4mstein/moviedbdemo/modules/login/LoginActivity.java
@@ -0,0 +1,43 @@
+package ua.r4mstein.moviedbdemo.modules.login;
+
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import ua.r4mstein.moviedbdemo.R;
+import ua.r4mstein.moviedbdemo.modules.base.BaseActivity;
+
+public class LoginActivity extends BaseActivity<LoginPresenter> {
+
+    private Toolbar mToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setHomeButtonEnabled(false);
+        getSupportFragmentManager().addOnBackStackChangedListener(() ->
+                getSupportActionBar().setDisplayHomeAsUpEnabled(getSupportFragmentManager().getBackStackEntryCount() > 0)
+        );
+
+    }
+
+    @Override
+    protected LoginPresenter initPresenter() {
+        return new LoginPresenter();
+    }
+
+    @Override
+    protected int getLayoutResource() {
+        return R.layout.activity_login;
+    }
+
+    @Override
+    protected void findUI() {
+        mToolbar = (Toolbar) findViewById(R.id.toolbar_AL);
+    }
+
+    @Override
+    protected void setupUI() {
+        setSupportActionBar(mToolbar);
+    }
+}
---
+++ app/src/main/java/ua/r4mstein/moviedbdemo/modules/login/LoginPresenter.java
@@ -0,0 +1,11 @@
+package ua.r4mstein.moviedbdemo.modules.login;
+
+import ua.r4mstein.moviedbdemo.modules.base.BaseActivityPresenter;
+
+public class LoginPresenter extends BaseActivityPresenter {
+
+    @Override
+    public void onViewCreated() {
+        super.onViewCreated();
+    }
+}
